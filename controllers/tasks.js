a427699bf19ad4ebabbb2375423287d842b2eafb
Abdelhakim mohamed <70428788+HakimMohamed@users.noreply.github.com>
2023-07-03 15:15:54 +0300

    Added test var

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,7 +3,7 @@ const asyncWrapper = require('../middleware/async')
 const { createCustomError } = require('../errors/custom-error')
 
 
-
+let test = "test1234"
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({})
   res.status(200).json({ tasks })
